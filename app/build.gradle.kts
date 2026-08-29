f246afae337332303cc3120831e6a80a79f28f3e
nelalemento-max <nelalemento@gmail.com>
2026-08-29 02:36:02 -0400

    camera: add EXIF support for captured gauge images

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,4 +64,5 @@ dependencies {
     implementation("androidx.camera:camera-camera2:1.4.1")
     implementation("androidx.camera:camera-lifecycle:1.4.1")
     implementation("androidx.camera:camera-view:1.4.1")
+    implementation("androidx.exifinterface:exifinterface:1.3.7")
 }
